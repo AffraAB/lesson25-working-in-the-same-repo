c63c86aebba8bfd0e99cd21340837d78792719d7
Nicholas-Chill <70136448+Nicholas-Chill@users.noreply.github.com>
2023-06-12 13:20:31 +0200

    Update first.js

--- first.js
+++ first.js
@@ -4,4 +4,4 @@ console.log("i was here first");
 console.log("i was here first");
 console.log("i was here first");
 console.log("i was here first");
-console.log("No you were not Viktor! I was the here first!);
+console.log("No you were not Viktor! I was the here first!");
